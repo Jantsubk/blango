05401022ae2ed6ecfc34a10beac81d44af321a6a
Janita Kaski <janita.kaski@metropolia.fi>
2023-12-17 15:36:28 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -6,7 +6,9 @@ from blog.models import Post
 import logging
 
 user_model = get_user_model()
+
 logger = logging.getLogger(__name__)
+logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
 
 from django import template
 
